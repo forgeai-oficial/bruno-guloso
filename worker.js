baf35369264b96fc80ab0c9c6c7482e7ad9999fe
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 02:00:00 -0300

    Load isolated test-flight v3 and keyboard repair

--- worker.js
+++ worker.js
@@ -117,7 +117,8 @@ export default {
               '<script src="/mobile-arrow-tune.js?v=1"></script>' +
               '<script src="/mobile-full-touch.js?v=3"></script>' +
               '<script src="/mobile-invisible-arrows.js?v=3"></script>' +
-              '<script src="/test-flight.js?v=2"></script>',
+              '<script src="/test-flight.js?v=3"></script>' +
+              '<script src="/keyboard-repair.js?v=1"></script>',
               { html: true },
             );
           },
