bf9f4714efd2db2fd63433da779f6d8c41cfe252
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 01:09:05 -0300

    Disable moving mobile crop and use static full-height framing

--- worker.js
+++ worker.js
@@ -112,7 +112,7 @@ export default {
               '<script src="/mobile-controls.js?v=3"></script>' +
               '<script src="/finish-pro.js?v=1"></script>' +
               '<script src="/orientation-game-only.js?v=1"></script>' +
-              '<script src="/mobile-camera-framing.js?v=1"></script>',
+              '<script src="/mobile-static-fit.js?v=1"></script>',
               { html: true },
             );
           },
