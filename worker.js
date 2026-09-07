256b776ddc447bb80ee626861ffcf13d5916b92f
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 01:37:43 -0300

    Bust mobile control cache for gapless forward 2x2 grid

--- worker.js
+++ worker.js
@@ -115,8 +115,8 @@ export default {
               '<script src="/mobile-static-fit.js?v=1"></script>' +
               '<script src="/mobile-side-viewport.js?v=1"></script>' +
               '<script src="/mobile-arrow-tune.js?v=1"></script>' +
-              '<script src="/mobile-full-touch.js?v=2"></script>' +
-              '<script src="/mobile-invisible-arrows.js?v=2"></script>',
+              '<script src="/mobile-full-touch.js?v=3"></script>' +
+              '<script src="/mobile-invisible-arrows.js?v=3"></script>',
               { html: true },
             );
           },
