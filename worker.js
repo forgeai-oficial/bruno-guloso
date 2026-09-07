9ff338befcd720654fc88a44b2ebef454475ec7b
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 01:25:53 -0300

    Load reliable mobile jump touch and invisible arrow pads

--- worker.js
+++ worker.js
@@ -115,7 +115,8 @@ export default {
               '<script src="/mobile-static-fit.js?v=1"></script>' +
               '<script src="/mobile-side-viewport.js?v=1"></script>' +
               '<script src="/mobile-arrow-tune.js?v=1"></script>' +
-              '<script src="/mobile-full-touch.js?v=1"></script>',
+              '<script src="/mobile-full-touch.js?v=2"></script>' +
+              '<script src="/mobile-invisible-arrows.js?v=1"></script>',
               { html: true },
             );
           },
