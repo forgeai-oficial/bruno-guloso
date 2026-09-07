81ca20f1f855ece4c119c569f77ffe46a4842c94
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 03:15:55 -0300

    Load donut touch fix and mushroom mark v5

--- worker.js
+++ worker.js
@@ -131,8 +131,9 @@ export default {
               '<script src="/mobile-arrow-tune.js?v=1"></script>' +
               '<script src="/mobile-full-touch.js?v=4"></script>' +
               '<script src="/mobile-invisible-arrows.js?v=5"></script>' +
-              '<script src="/mushroom-tip-mark.js?v=4"></script>' +
+              '<script src="/mushroom-tip-mark.js?v=5"></script>' +
               '<script src="/donut-score-powerup.js?v=3"></script>' +
+              '<script src="/donut-touch-fix.js?v=1"></script>' +
               '<script src="/composite-score-ui.js?v=1"></script>' +
               '<script src="/test-flight.js?v=3"></script>',
               { html: true },
