72a30f28bcf2a3fa084efe68d055fbca32e249fb
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 01:17:18 -0300

    Show real map in mobile side areas

--- worker.js
+++ worker.js
@@ -112,7 +112,8 @@ export default {
               '<script src="/mobile-controls.js?v=3"></script>' +
               '<script src="/finish-pro.js?v=1"></script>' +
               '<script src="/orientation-game-only.js?v=1"></script>' +
-              '<script src="/mobile-static-fit.js?v=1"></script>',
+              '<script src="/mobile-static-fit.js?v=1"></script>' +
+              '<script src="/mobile-side-viewport.js?v=1"></script>',
               { html: true },
             );
           },
