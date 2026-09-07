d6813f4ee7e611a2fb7834f6490b842a627dda90
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 01:54:18 -0300

    Load test flight mode with ranking disabled

--- worker.js
+++ worker.js
@@ -116,7 +116,8 @@ export default {
               '<script src="/mobile-side-viewport.js?v=1"></script>' +
               '<script src="/mobile-arrow-tune.js?v=1"></script>' +
               '<script src="/mobile-full-touch.js?v=3"></script>' +
-              '<script src="/mobile-invisible-arrows.js?v=3"></script>',
+              '<script src="/mobile-invisible-arrows.js?v=3"></script>' +
+              '<script src="/test-flight.js?v=1"></script>',
               { html: true },
             );
           },
