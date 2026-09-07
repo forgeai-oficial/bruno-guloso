6bf660476acd795d45cfe1e1ea492509738525b7
forgeai-oficial <forgeai.oficial@gmail.com>
2026-09-07 04:10:05 -0300

    deploy: load definitive mushroom marks and slower movement

--- worker.js
+++ worker.js
@@ -338,12 +338,12 @@ export default {
               '<script src="/mobile-arrow-tune.js?v=1"></script>' +
               '<script src="/mobile-full-touch.js?v=4"></script>' +
               '<script src="/mobile-invisible-arrows.js?v=5"></script>' +
-              '<script src="/mushroom-tip-mark.js?v=7"></script>' +
+              '<script src="/mushroom-tip-mark.js?v=8"></script>' +
               '<script src="/donut-score-powerup.js?v=3"></script>' +
               '<script src="/donut-touch-fix.js?v=2"></script>' +
               '<script src="/blue-brick-score-v4.js?v=2"></script>' +
               '<script src="/composite-score-ui.js?v=2"></script>' +
-              '<script src="/speed-v1-lock.js?v=2"></script>' +
+              '<script src="/speed-v1-lock.js?v=3"></script>' +
               '<script src="/access-analytics.js?v=1"></script>' +
               '<script src="/test-flight.js?v=3"></script>',
               { html: true },
